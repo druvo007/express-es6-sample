f92599099ae10f3f326e8d4ffa724aea322ff879
druvo007 <100901674+druvo007@users.noreply.github.com>
2025-11-08 01:07:09 +0800

    index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'World' });
+  res.render('index', { title: 'My Student ID is 21988912. Welcome to Express' });
 });
 
 export default router;
